d40e7f2e8e5ce6cd47a0dd5ead161f8a87ca9fa0
vibesharing <buret.hadrien@orange.fr>
2017-09-15 09:15:50 +0900

    map without logic

--- cropchat/src/main.js
+++ cropchat/src/main.js
@@ -6,9 +6,17 @@ import router from './router'
 import Vuefire from 'vuefire'
 import VueResource from 'vue-resource'
 import firebase from './service/firebase'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(Vuefire)
 Vue.use(VueResource)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAd7k4cerxYQXnydcxCzV0TCx8qFupHBPc',
+    v: '3',
+    libraries: 'places'
+  }
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- cropchat/src/router/index.js
+++ cropchat/src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import HomeView from '@/components/HomeView'
 import DetailView from '@/components/DetailView'
 import PostView from '@/components/PostView'
+import MapView from '@/components/MapView'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/post',
       name: 'post',
       component: PostView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
     }
   ]
 })
